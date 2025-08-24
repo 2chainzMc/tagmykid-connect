3648383e31c6c0e556626761aec3e12a7df11cd8
2chainzMc <thulaning62@gmail.com>
2025-08-25 00:25:01 +0200

    hash upgrades

--- src/App.tsx
+++ src/App.tsx
@@ -101,7 +101,6 @@ const App = () => (
       <Toaster />
       <Sonner />
       <AuthProvider>
-        {/* Switch to HashRouter for GitHub Pages */}
         <HashRouter>
           <AppRoutes />
         </HashRouter>
